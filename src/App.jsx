14236cc0bf34f42ecd429105de328d9e485cd165
josafat <josa221001@gmail.com>
2023-07-12 18:48:31 -0600

    add obtener ubicacion del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  const getLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        },
+        (error) => {
+          console.log("No se pudo obtener la ubicacion");
+        }
+      );
+    }
+  };
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-teal-500">Hola mundo</h1>
+      <h1 className="text-3xl font-bold text-teal-500">Formulario</h1>
+
+      <button onClick={getLocation}>Obtener ubicacion</button>
+
+      <h2>
+        Ubicacion: {lat}, {long}
+      </h2>
+      <div></div>
     </>
   );
 }
