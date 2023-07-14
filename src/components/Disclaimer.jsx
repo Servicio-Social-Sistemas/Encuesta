f9e4b4e8e676db050dedd03558dc0ecd6a2a7df2
josafat <josa221001@gmail.com>
2023-07-14 16:38:34 -0600

    add: creating disclaimer component

---
+++ src/components/Disclaimer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Disclaimer() {
+  return (
+    <div>
+      <h2>Indicaciones</h2>
+      <p>
+        Para conestar esta encuesta se necesita tener acceso a su ubicación con
+        fines estadisticos.
+      </p>
+    </div>
+  );
+}
+
+export default Disclaimer;
