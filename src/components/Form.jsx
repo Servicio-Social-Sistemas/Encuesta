0b3db09cbb216664c545cbb19a8a369b1fe86963
josafat <josa221001@gmail.com>
2023-07-14 17:27:12 -0600

    add: using ubication at form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import { useState } from "react";
 
 function Form() {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  const getLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        },
+        (error) => {
+          console.log("No se pudo obtener la ubicacion");
+        }
+      );
+    }
+  };
+
+  const UserData = {
+    response: {
+      respuesta1: "",
+    },
+    ubication: {
+      latitud: lat,
+      longitud: long,
+    },
+  };
+
+  console.log(UserData);
+
   return (
     <form className="my-4">
       <h2 className="font-bold uppercase">Preguntas</h2>
       {/* Aqui va el contenido del formulario */}
-
       <button className="bg-pink-400 py-2 px-10 rounded-md text-white hover:bg-pink-500 duration-100">
         Enviar
       </button>
