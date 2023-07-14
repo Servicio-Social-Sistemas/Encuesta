a85baabbd08bea751cc83d313fa089efa0a73e84
josafat <josa221001@gmail.com>
2023-07-14 17:07:15 -0600

    update: information about ubication

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Disclaimer() {
   return (
-    <div>
-      <h2>Indicaciones</h2>
-      <p>
+    <div className="bg-indigo-400 rounded-sm p-2 text-white max-w-sm mx-auto">
+      <h2 className="font-bold">Indicaciones</h2>
+      <p className="text-justify">
         Para conestar esta encuesta se necesita tener acceso a su ubicación con
         fines estadisticos.
       </p>
