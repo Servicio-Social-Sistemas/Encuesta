5ff0de7cbc5f27771529a0d3e1db0d1147ac739d
josafat <josa221001@gmail.com>
2023-07-14 17:07:54 -0600

    add: form estructure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <form className="my-4">
+      <h2 className="font-bold uppercase">Preguntas</h2>
+      {/* Aqui va el contenido del formulario */}
+
+      <button className="bg-pink-400 py-2 px-10 rounded-md text-white hover:bg-pink-500 duration-100">
+        Enviar
+      </button>
+    </form>
+  );
 }
 
 export default Form;
