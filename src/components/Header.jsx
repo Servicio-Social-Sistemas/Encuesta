3f2ebc695178a8f6fe23ebe2af5348e7fd2296ff
josafat <josa221001@gmail.com>
2023-08-09 14:50:26 -0600

    rmv: eliminando el logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Logo from "../assets/logo.png";
 
 function Header() {
   return (
     <header className="p-4 flex align-middle justify-center mb-4 border-b-8 pb-3 border-[#483285] md:pb-5">
-      <img
-        className="max-w-lg max-h-14"
-        src={Logo}
-        alt="Logo gobierno municipal de puebla"
-      />
+      <h1 className="text-3xl font-bold">Encuesta</h1>
     </header>
   );
 }
